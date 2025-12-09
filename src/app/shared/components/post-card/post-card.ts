035c2021f9b4b716cb409663d5900b60e119aeca
AdrianSiwek333 <adisiwek@gmail.com>
2025-12-09 17:40:17 +0100

    Add post component

---
+++ src/app/shared/components/post-card/post-card.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Post } from '../../../core/models/post.model';
+
+@Component({
+  selector: 'app-post-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './post-card.html',
+  styleUrls: ['./post-card.css']
+})
+export class PostCardComponent {
+  @Input() post!: Post;
+
+  onLike() {
+    console.log('Polubiono post ID:', this.post.id);
+  }
+}
