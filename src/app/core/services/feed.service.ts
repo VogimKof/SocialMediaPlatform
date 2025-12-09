8a7fe816461ed3ae3ece5dab4a230f7cb95cf3ca
AdrianSiwek333 <adisiwek@gmail.com>
2025-12-09 17:30:50 +0100

    add feed service

---
+++ src/app/core/services/feed.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Post } from '../models/post.model';
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FeedService {
+  private postsUrl = 'assets/mock-data/posts.json';
+  private contactsUrl = 'assets/mock-data/contacts.json';
+
+  constructor(private http: HttpClient) {}
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.postsUrl);
+  }
+
+  getContacts(): Observable<User[]> {
+    return this.http.get<User[]>(this.contactsUrl);
+  }
+}
