33079ff70cd018d1a581931c895a9538c9dfcb5d
AdrianSiwek333 <adisiwek@gmail.com>
2026-01-17 18:42:30 +0100

    feat: add route guards for authentication; implement guest and auth guards for route protection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { RegisterComponent } from './features/register/register.component';
 import { LoginComponent } from './features/login/login.component';
 import { FeedComponent } from './features/feed/feed';
 import { Profile } from './features/profile/profile';
+import { guestGuard } from './core/guards/guest-guard';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'feed', component: FeedComponent } ,
-  { path: 'profile/:id', component: Profile }
+  { path: 'login', component: LoginComponent, canActivate: [guestGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [guestGuard] },
+  { path: 'feed', component: FeedComponent, canActivate: [authGuard] },
+  { path: 'profile/:id', component: Profile, canActivate: [authGuard] }
 ];
